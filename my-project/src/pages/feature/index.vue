d51ebb384717553d9a49468ac9d631e2f945be25
student <student@hubu.edu.cn>
2019-05-29 11:31:52 +0800

    food界面的超长文本写好啦啦啦啦啦啦

--- my-project/src/pages/feature/index.vue
+++ my-project/src/pages/feature/index.vue
@@ -1,24 +1,236 @@
 <template>
   <div > 
         
-            <view class="box head">乌江源上的秘密</view>
+            <view class="box head">舌尖上的贵州那些不容错过的黔味美食</view>
             <view class="first">
              <text decode="true" space="true">&nbsp;&nbsp;
-             乌江源百里画廊旅游区属国家级旅游资源。\n</text>
+              在贵州这片神奇的士地上，极大的地域差异和丰富的少数民
+              族文化为其美食造就了一片广阔的天地。书有记载:贵州的
+              珍馐美味并非自然天成，而是得益于人类的点化，其上应天
+              时，下得地利，中取人和。因此，舌尖上的贵州是一种融
+              合，更是一种和谐。快去贵州体验地道的美食吧!无论是浓
+              郁“名族风”，还是随意“小吃风”，无论是名厨大菜还是街头
+              饭馆，贵州的美食定能让你的舌尖Hold不住，大呼过瘾!
+              \n</text>
             </view>
              <text>\n</text>
 
-            <view class="text">
-            <text  decode="true" space="true">&nbsp;&nbsp;
-            位于黔西县南部，321国道公路旁，因修建东风水电站而形成。
-            湖区总长62千米，宽度60-1000米之间，湖水面积近20平方千米。
-            湖水清澄，倒影沉碧，宁静秀丽,两岸峰壁险峻，气势恢弘，且多瀑布山泉跌落湖中，
-            是千里乌江上最美的崖壁画廊。景区交通方便，距省城贵阳80千米，距黔西城33千米，
-            水路连通国家级风景名胜区织金洞，形成贵阳西部风光一绝。\n</text>
+           <img src="/static/tabs/timg.jpg" width="100%" height="200px">
+          <view class="text">
+           <text  decode="true" space="true">&nbsp;&nbsp;
+              豆腐果\n</text>
             <text>\n</text>
-            </view>
+            <text  decode="true" space="true">&nbsp;&nbsp;
+            恋爱豆腐果，听起来十分浪漫的名字，贵阳的有名的风
+            味小吃。它的主料是切成长方形小块的白豆腐。制作时将铁
+            制烤床上抹油，下用糠壳作燃料，将豆腐块放在铁烤床上后
+            不停翻动，至皮色黄亮即可。吃时还另配内装有酱油、味
+            精、香醋、胡辣椒、香油、葱花、姜未等佐料的小碟，趁热
+            蘸着吃。其特点是外脆内嫩、咸辣爽滑，满口喷香，而且价
+            廉物美、携带方便、节省时间，是风靡贵州的大众小吃
+            \n</text>
+            <text>\n</text>
+          </view>
+
+           <img src="/static/tabs/timg.jpg" width="100%" height="200px">
+          <view class="text">
+           <text  decode="true" space="true">&nbsp;&nbsp;
+            洋芋粑\n</text>
+            <text>\n</text>
+            <text  decode="true" space="true">&nbsp;&nbsp;
+            这是贵州街头的小吃摊都有的一种特色小吃，走近看到
+            被烙得金黄，老远就能闻得士豆和葱花的混合香味，特别的
+            诱人，也顾不得是在小推子边的不雅，大饱口福为快。 \n</text>
+            <text>\n</text>
+          </view>
+           <img src="/static/tabs/timg.jpg" width="100%" height="200px">
+
+          <view class="text">
+           <text  decode="true" space="true">&nbsp;&nbsp;
+            荞灰粽子\n</text>
+            <text>\n</text>
+            <text  decode="true" space="true">&nbsp;&nbsp;
+            荞灰粽子是贵州省黔南州特有的一种粽子，粽子叶的味道和
+            糯米草的味道很单纯的混合在一起，吃起来口感细腻，而且
+            带着清香味…灰粽子的制作过程:先将糯米草烧成
+            灰，然后用草灰与糯米拌好，撒一点水，然后用手搓，直到
+            草灰将糯米染成黑色，然后将草灰去掉，这样就可以包了
+            你可以根据自己的口味添加其他的材料，有的喜欢加花椒
+            汁，有的喜欢加腊肉.…，不过我们在这里推荐不要添加其
+            他材料料，让粽子叶的味道和糯米草的味道很单纯的混合在
+            起，吃起来口感细腻，而且带着清香味。\n</text>
+            <text>\n</text>
+          </view> 
+           <img src="/static/tabs/timg.jpg" width="100%" height="200px">         
           
-            <img src="/static/tabs/timg.jpg" width="100%" height="200px">
+           <view class="text">
+           <text  decode="true" space="true">&nbsp;&nbsp;
+            杨姨妈丝娃娃\n</text>
+            <text>\n</text>
+            <text  decode="true" space="true">&nbsp;&nbsp;
+           进店先洗个手，然后端坐于木头小板凳上，面前七盘八
+            碟摆开阵势，面皮推在手中，举起筷子“焦人"地这个盘里夹
+            点，那个盘里夹一点，厉害的是每个盘里都要夹一点，然
+            后，就开始集中展现贵州人“包容"的技术含量一面皮包
+            起那些内容，小心地包，要像打包婴儿一样的小心(丝娃娃
+            的名字就是打这儿来的吧)，包成个襁褓，确定底部不会漏
+            汤，就用小调羹舀上辣椒水往那小包袱里灌，接下来是最精
+            彩的画面，无论嘴巴大或小，你得在汤汁浸出面皮之前把那
+            包东西全部塞进嘴里，然后包口包嘴地咀嚼至吞下。以此方
+            式，反复操作，成年人一般可轮回20次，外加一碗绿豆汤一
+            碗盐菜饭也不在话下，而且结束之时定是神清气爽、心满意
+            足。\n</text>
+            <text>\n</text>
+          </view>
+           <img src="/static/tabs/timg.jpg" width="100%" height="200px">
+
+          <view class="text">
+           <text  decode="true" space="true">&nbsp;&nbsp;
+           豌豆粉\n</text>
+            <text>\n</text>
+            <text  decode="true" space="true">&nbsp;&nbsp;
+            豌豆粉在清朝未年已有出售，民国时期遍及全县。原为春
+            夏小吃近年来在县城基本上四季畅销。城内的凉粉推点是年
+            轻女性最爱光顾的地方。\n</text>
+            <text>\n</text>
+          </view>
+           <img src="/static/tabs/timg.jpg" width="100%" height="200px">
+
+          <view class="text">
+           <text  decode="true" space="true">&nbsp;&nbsp;
+           凉拌卷粉\n</text>
+            <text>\n</text>
+            <text  decode="true" space="true">&nbsp;&nbsp;
+            卷粉是贵州传统的美食，当地人可以每天都吃卷粉当作
+            早餐都不会厌倦，其美味和引诱度不言而喻。\n</text>
+            <text>\n</text>
+          </view>
+           <img src="/static/tabs/timg.jpg" width="100%" height="200px">
+
+          <view class="text">
+           <text  decode="true" space="true">&nbsp;&nbsp;
+           胡记牛肉粉\n</text>
+            <text>\n</text>
+            <text  decode="true" space="true">&nbsp;&nbsp;
+            胡记"招牌上标明了来自安顺。貌似贵州其它地区的牛
+            肉粉无论清炖还是红烧，汤里面都不放辣椒，所以牛肉粉也
+            是不辣的，嗜辣的食客需要自己放干辣椒面或油辣椒。\n</text>
+            <text>\n</text>
+          </view>
+           <img src="/static/tabs/timg.jpg" width="100%" height="200px">
+
+           <view class="text">
+           <text  decode="true" space="true">&nbsp;&nbsp;
+           王记花溪牛肉粉\n</text>
+            <text>\n</text>
+            <text  decode="true" space="true">&nbsp;&nbsp;
+            花溪王记牛肉粉'创业于1976年，走过了磋砣的二十五
+            个年头。其独特秘制的汤料，上好的贵州本地自然放养的黄
+            牛肉，精细的工艺，以及严格的制作规程-煮、吊、卤
+            兑、泡等。\n</text>
+            <text>\n</text>
+          </view>
+           <img src="/static/tabs/timg.jpg" width="100%" height="200px">
+
+          <view class="text">
+           <text  decode="true" space="true">&nbsp;&nbsp;
+           师赐福肠旺面\n</text>
+            <text>\n</text>
+            <text  decode="true" space="true">&nbsp;&nbsp;
+            肠旺面又称肠益面，是贵州极负盛名的一种汉族风味面
+            食。在贵州众多的小吃中，以色、香、味“三绝"而著称，具
+            有血嫩、面脆、辣香、汤鲜的风味和口感，以及红而不辣、
+            油而不腻、脆而不生的特点。肠"即猪大肠，“"旺"则是猪血
+            加上面条，三者相加便相得益章。“肠旺"是“常旺"的谐音，
+            寓意吉祥。贵阳市区内各小吃街均有卖。\n</text>
+            <text>\n</text>
+          </view>
+           <img src="/static/tabs/timg.jpg" width="100%" height="200px">
+
+          <view class="text">
+           <text  decode="true" space="true">&nbsp;&nbsp;
+           辣子鸡\n</text>
+            <text>\n</text>
+            <text  decode="true" space="true">&nbsp;&nbsp;
+            贵州辣子鸡是一道非常有特色的菜。相比湖南、重庆等
+            地用干辣椒炸出来的辣子鸡，我更爱贵州辣子鸡。它是由特
+            色的糍粑辣椒与鸡肉一起炒制而成，两种食材的味道相互交
+            在一起，没有那种川版和湘版的火爆，却更加绵绵悠长，
+            刺激着你的味蕾，好吃的让你停不下口。\n</text>
+            <text>\n</text>
+          </view>
+           <img src="/static/tabs/timg.jpg" width="100%" height="200px">
+
+          <view class="text">
+           <text  decode="true" space="true">&nbsp;&nbsp;
+           豆沙窝\n</text>
+            <text>\n</text>
+            <text  decode="true" space="true">&nbsp;&nbsp;
+            这是一种油炸糯食，在糍粑中放入豆沙馅，用油烹炸而
+            成，具有色泽金黄、外酥内软、糯香爽口的特点。馅心成
+            鲜，老少皆宜。贵阳清早街头巷尾都有卖，一张桌子，架一
+            油锅，现做现炸，人们老远就闻到浓浓的糯米香，路人走上
+            前丢下一元钱拿起一个边走边吃，也有买了回家当早餐。外
+            脆里糯，喜甜爱咸都可得到满足，吃完满口留香。\n</text>
+            <text>\n</text>
+          </view>
+           <img src="/static/tabs/timg.jpg" width="100%" height="200px">
+
+          <view class="text">
+           <text  decode="true" space="true">&nbsp;&nbsp;
+           但家香酥鸭\n</text>
+            <text>\n</text>
+            <text  decode="true" space="true">&nbsp;&nbsp;
+            多年绝技、麻得开心。"以鲜麻酥脆、清香回味为特色
+            的但家香酥鸭，历经20余年的经营打拼，如今已成长为贵阳
+            市民耳熟能详的美食。\n</text>
+            <text>\n</text>
+          </view>
+           <img src="/static/tabs/timg.jpg" width="100%" height="200px">
+
+          <view class="text">
+           <text  decode="true" space="true">&nbsp;&nbsp;
+           豆豉火锅\n</text>
+            <text>\n</text>
+            <text  decode="true" space="true">&nbsp;&nbsp;
+            这豆豉不是一般的豆豉，是豆豉的提炼，浓缩中的浓
+            缩，当地叫“豆豉粑”，看起来跟年糕似的，不过是黑色的方
+            块，打开一闻，别说臭，那是香，那是臭得很香。豆豉粑只
+            需切下薄薄的一小片，放在制熟的菜油锅里，这一锅豆豉火
+            锅顿时就有了华丽的贵族火锅血统，那香味在你呼吸的空间
+            里瞬间爆炸，像核武器一样四处扩散，惊得街坊四邻纷纷推
+            开窗户探出脑袋，闭着眼睛深深呼吸一口，轻轻叹一声:
+            香!然后打发孩子到菜场买一块豆豉粑回来，今晚咱家也吃豆豉火锅。\n</text>
+            <text>\n</text>
+          </view>
+           <img src="/static/tabs/timg.jpg" width="100%" height="200px">
+
+          <view class="text">
+           <text  decode="true" space="true">&nbsp;&nbsp;
+           黄家老糯食\n</text>
+            <text>\n</text>
+            <text  decode="true" space="true">&nbsp;&nbsp;
+            糯米饭是贵阳比较喜爱的一种糯食，黄家老糯食店已经开
+            了十多年，糯米颗颗饱满，加上折耳根、油辣椒等作料，
+            点都不会腻。\n</text>
+            <text>\n</text>
+          </view>
+           <img src="/static/tabs/timg.jpg" width="100%" height="200px">
+
+          <view class="text">
+           <text  decode="true" space="true">&nbsp;&nbsp;
+           烤脑花\n</text>
+            <text>\n</text>
+            <text  decode="true" space="true">&nbsp;&nbsp;
+            烤脑花原是重庆地区的一道特色美味小吃，采用猪脑
+            子，扮之以海椒末、花椒粉，以及其他佐料，火烤而成。气
+            味醇厚、入口绵软、香而不腻、油而不肥。后传到贵阳，经
+            创新改良成为更符合贵阳人口味的小吃，是到贵阳旅游时切
+            不可遗漏的一道美食。\n</text>
+            <text>\n</text>
+          </view>
+           <img src="/static/tabs/timg.jpg" width="100%" height="200px">
+
             <view class="text">
               <text  decode="true" space="true">&nbsp;&nbsp;
               乌江是长江南岸最大的一级支流，全长1050公里,乌江有南、北两条河，南源三岔河发源于威宁县的香炉山，
@@ -88,7 +300,7 @@ img{
 .box{
   display:block;
   width:400px;
-  height:40px;
+  height:60px;
   border-bottom:1px solid #EEE9E9;
   padding-top:10px;
 }
